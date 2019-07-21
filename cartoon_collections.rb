64a14e5e5c5c7601048a59eb985de4e8d66848b2
Kevin Bagnall <bubbagump2020@gmail.com>
2019-07-21 18:47:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,7 @@ def long_planeteer_calls# code an argument here
   # Your code here
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(cheese)
+
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
